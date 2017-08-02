a9ac99bedca702ad3a1c5059f8216156acc35a8c
Tashfiqul Bari <tashfiqulbari@gmail.com>
2017-08-02 19:37:37 +0600

    First Game

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance;
+
+	void Awake(){
+
+		if (Instance == null)
+			Instance = this.GetComponent<GameManager> ();
+	}
+		
+}
